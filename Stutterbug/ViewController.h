583cadd74237919111b99b39e8ecc8778058939e
邵建勇 <John_Shaw@Johns-MacBook-Air.local>
2014-10-24 12:56:59 +0800

    Initial Commit

---
+++ Stutterbug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stutterbug
+//
+//  Created by 邵建勇 on 14/10/24.
+//  Copyright (c) 2014年 John Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Stutterbug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stutterbug
+//
+//  Created by 邵建勇 on 14/10/24.
+//  Copyright (c) 2014年 John Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
